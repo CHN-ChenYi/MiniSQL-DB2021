1d482fd1f4113e196c30354588a8fe9523febdf9
yangrq <3190105025@zju.edu.cn>
2021-06-05 16:39:57 +0800

    change some configure, add part of SQL parser

--- src/Interpreter/Interpreter.cc
+++ src/Interpreter/Interpreter.cc
@@ -1,5 +1,115 @@
 #include "Interpreter.hpp"
 
+#include <algorithm>
+#include <atomic>
+#include <cctype>
+#include <iostream>
+#include <iterator>
+#include <regex>
+
+using std::cerr;
+using std::endl;
+using std::isalnum;
+using std::isalpha;
+using std::min;
+using std::string_view;
+
+using namespace std::literals;
+
 void Interpreter::Interpret() const {}
+
+void Interpreter::handleError(std::string_view msg, InputType pos) const {
+  cerr << msg << endl;
+}
+
+Interpreter::ParserState Interpreter::skipSpace(
+    Interpreter::InputType input) const {
+  input.remove_prefix(min(input.find_first_not_of(" \n\r\t"), input.size()));
+  return {true, TokenNone, input};
+}
+
+Interpreter::ParserState Interpreter::expect(
+    string_view lit, Interpreter::InputType input) const {
+  auto [dummy1, dummy2, rest] = skipSpace(input);
+  if (rest.starts_with(lit)) {
+    rest.remove_prefix(lit.size());
+    return {true, Token{.kind = TokenKind::KeyWord, .sv = lit}, rest};
+  } else {
+    return {false, TokenNone, rest};
+  }
+}
+
+Interpreter::ParserState Interpreter::peek(string_view lit,
+                                           Interpreter::InputType input) const {
+  auto [dummy1, dummy2, rest] = skipSpace(input);
+  if (rest.starts_with(lit)) {
+    return {true, Token{.kind = TokenKind::KeyWord, .sv = lit}, rest};
+  } else {
+    return {false, TokenNone, rest};
+  }
+}
+
+Interpreter::ParserState Interpreter::parseNumber(
+    Interpreter::InputType input) const {
+  auto [dummy1, dummy2, rest] = skipSpace(input);
+  // TODO
+  return {false, TokenNone, rest};
+}
+
+Interpreter::ParserState Interpreter::parseId(
+    Interpreter::InputType input) const {
+  auto [dummy1, dummy2, rest] = skipSpace(input);
+  if (!rest.empty())
+    if (isalpha(rest[0])) {
+      size_t i = 1;
+      for (; i < rest.size() && isalnum(rest[i]); ++i)
+        ;
+      auto tok = rest.substr(0, i);
+      rest.remove_prefix(i);
+      return {true, Token{.kind = TokenKind::Id, .sv = tok}, rest};
+    }
+  return {false, TokenNone, rest};
+}
+
+Interpreter::ParserState Interpreter::parsePath(
+    Interpreter::InputType input) const {
+  auto [dummy1, dummy2, rest] = skipSpace(input);
+  // TODO
+  return {false, TokenNone, rest};
+}
+
+Interpreter::ParserState Interpreter::parseCreateTable(
+    Interpreter::InputType input) const {
+  auto [flag1, _1, rest1] = expect("create"sv, input);
+  if (!flag1) {
+    handleError("expect token `create`", rest1);
+    return {false, TokenNone, rest1};
+  }
+  auto [flag2, _2, rest2] = expect("table"sv, rest1);
+  if (!flag2) {
+    handleError("expect token `table`", rest2);
+    return {false, TokenNone, rest2};
+  }
+  auto [flag3, id, rest3] = parseId(rest2);
+  if (!flag3) {
+    handleError("expect valid identifier like `[a-zA-Z][a-zA-Z0-9]*`", rest3);
+    return {false, TokenNone, rest3};
+  }
+  // TODO
+}
+Interpreter::ParserState Interpreter::parseCreateIndex(
+    Interpreter::InputType input) const {}
+Interpreter::ParserState Interpreter::parseSelectStat(
+    Interpreter::InputType input) const {}
+Interpreter::ParserState Interpreter::parseDeleteStat(
+    Interpreter::InputType input) const {}
+Interpreter::ParserState Interpreter::parseInsertStat(
+    Interpreter::InputType input) const {}
+Interpreter::ParserState Interpreter::parseDropTable(
+    Interpreter::InputType input) const {}
+Interpreter::ParserState Interpreter::parseDropIndex(
+    Interpreter::InputType input) const {}
+Interpreter::ParserState Interpreter::parseExec(
+    Interpreter::InputType input) const {}
 
 Interpreter interpreter;
--- src/Interpreter/Interpreter.hpp
+++ src/Interpreter/Interpreter.hpp
@@ -1,13 +1,73 @@
 #pragma once
+#include <optional>
+#include <string>
+#include <string_view>
+#include <tuple>
+#include <unordered_set>
+#include <utility>
 
 class Interpreter {
  public:
+  using InputType = std::string_view;
   /**
    * @brief Read commands from stdin, interpret them and then execute them by
    * API module. Return when user quits.
    *
    */
   void Interpret() const;
+
+ private:
+  /*
+  static inline constexpr InputType keywords[] = {
+      "select", "insert",  "create", "drop",     "delete", "table", "index",
+      "from",   "where",   "quit",   "execfile", "unique", "into",  "values",
+      "on",     "primary", "key",    "and",      "char",   "int",   "float",
+  };
+
+  static inline constexpr InputType cmpOpLiterals[] = {
+      "", "<=", ">=", "<>", "<", ">"};
+  */
+  enum class TokenKind {
+    None,
+    KeyWord,
+    CmpOp,
+    ArithOp,
+    Space,
+    INum,
+    RNum,
+    Id,
+    StrLit,
+    Path,
+  };
+
+  struct Token {
+    TokenKind kind;
+    union {
+      double f;
+      int i;
+      InputType sv;
+    };
+  };
+
+  static inline constexpr Token TokenNone = Token{.kind = TokenKind::None, .sv = ""};
+
+  using ParserState = std::tuple<bool, Token, InputType>;
+
+  void handleError(std::string_view msg, InputType pos) const;
+  ParserState skipSpace(InputType input) const;
+  ParserState expect(std::string_view lit, InputType input) const;
+  ParserState peek(std::string_view lit, InputType input) const;
+  ParserState parseNumber(InputType input) const;
+  ParserState parseId(InputType input) const;
+  ParserState parsePath(InputType input) const;
+  ParserState parseCreateTable(InputType input) const;
+  ParserState parseCreateIndex(InputType input) const;
+  ParserState parseSelectStat(InputType input) const;
+  ParserState parseDeleteStat(InputType input) const;
+  ParserState parseInsertStat(InputType input) const;
+  ParserState parseDropTable(InputType input) const;
+  ParserState parseDropIndex(InputType input) const;
+  ParserState parseExec(InputType input) const;
 };
 
 extern Interpreter interpreter;
